8812cabc82ccef5f6520eac85dd9d94872308c55
MuhamadZulhaqi24 <87411621+MuhamadZulhaqi24@users.noreply.github.com>
2022-01-25 10:14:37 +0700

    Sewa sesuai Antarmuka Figma

--- view/admin/sewa.php
+++ view/admin/sewa.php
@@ -44,22 +44,29 @@
       $db=dbConnect();
       if($db->connect_errno==0){
         $sql="SELECT  p.id_penghuni,
+                      k.nama_penghuni namapenghuni,
                       p.id_kamar,
                       p.id_penjaga,
                       p.tanggal_sewa,
                       p.tanggal_perpanjangan,
+                      i.harga_tahunan hargasewa,
                       p.status_lunas
-          FROM sewa p";
+          FROM sewa p 
+          JOIN penghuni k 
+          ON p.id_penghuni=k.id_penghuni
+          JOIN kamar i
+          on p.id_kamar=i.id_kamar";
+          
         $res=$db->query($sql);
         if($res){
           ?>
     <table class="table table-bordered">
       <tr class="bg-dark text-white" align="center">
-        <th>ID Penghuni</th>
+        <th>Nama Penghuni</th>
         <th>ID Kamar</th>
-        <th>ID Penjaga</th>
-        <th>Tanggal Sewa</th>
+        <th>Tanggal Masuk</th>
         <th>Tanggal Perpanjangan</th>
+        <th>Harga Sewa</th>
         <th>Status Lunas</th>
         <th>Aksi</th>
       </tr>
@@ -68,11 +75,11 @@
                   foreach($data as $datadiri){ // telusuri satu per satu
                     ?>
         <tr>
-          <td align="center"><?php echo $datadiri["id_penghuni"];?></td>
+          <td align="center"><?php echo $datadiri["namapenghuni"];?></td>
           <td align="center"><?php echo $datadiri["id_kamar"];?></td>
-          <td align="center"><?php echo $datadiri["id_penjaga"];?></td>
           <td align="center"><?php echo $datadiri["tanggal_sewa"];?></td>
           <td align="center"><?php echo $datadiri["tanggal_perpanjangan"];?></td>
+          <td align="center"><?php echo $datadiri["hargasewa"];?></td>
           <td align="center"><?php echo $datadiri["status_lunas"];?></td>
           <td><div align="center"><a href="crud_sewa/edit_sewa.php" class="btn btn-primary"><i class="fa fa-pen"></i></a></div></td>
           </tr>
