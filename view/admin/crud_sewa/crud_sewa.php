d9de2fb0fba4a06746a1a2675d10cfef5c33ea9f
Faisal <klsfaisalobara@gmail.com>
2022-01-27 23:01:04 +0700

    tambah data sewa backend + fix frontend

--- view/admin/crud_sewa/crud_sewa.php
+++ view/admin/crud_sewa/crud_sewa.php
@@ -34,4 +34,25 @@
 			header("Location: ../sewa.php?status=2");
 		}
 	}
+
+	if (isset($_POST["simpanTambahSewa"])) {
+		$db = dbConnect();
+		$tempIdPenghuni	= $db->escape_string($_POST["tempIdPenghuni"]);
+		$id_penghuni		= $db->escape_string($_POST["id_penghuni"]);
+		$id_kamar				= $db->escape_string($_POST["id_kamar"])		;
+		$id_penjaga			= $db->escape_string($_POST["id_penjaga"]	);
+		$tgl_masuk			= $db->escape_string($_POST["tgl_masuk"]);
+		$tgl_perpanjang	= $db->escape_string($_POST["tgl_perpanjang"]);
+		$status_lunas		= $db->escape_string($_POST["status_lunas"]);
+
+		$sql = "INSERT INTO sewa VALUES ('$id_penghuni','$id_kamar','$id_penjaga','$tgl_masuk','$tgl_perpanjang','$status_lunas')";
+
+		$res = $db->query($sql);
+
+		if ($db->affected_rows > 0) {
+			header("Location: ../sewa.php?status=1");
+		} else {
+			header("Location: ../sewa.php?status=2");
+		}
+	}
  ?>
